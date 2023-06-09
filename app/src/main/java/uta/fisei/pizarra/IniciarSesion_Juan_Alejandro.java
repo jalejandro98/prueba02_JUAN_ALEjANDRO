be695b4b693c46482b3107f7f0d5a38c43d2726f
jalejandro98 <juan-af98@hotmail.com>
2023-06-09 17:01:22 -0500

    Juan Alejandro: 17:01

--- app/src/main/java/uta/fisei/pizarra/IniciarSesion_Juan_Alejandro.java
+++ app/src/main/java/uta/fisei/pizarra/IniciarSesion_Juan_Alejandro.java
@@ -2,13 +2,29 @@ package uta.fisei.pizarra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IniciarSesion_Juan_Alejandro extends AppCompatActivity {
+
+    private Button button_ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion_juan_alejandro);
+
+        button_ingresar = findViewById(R.id.button_JUAN);
+
+        button_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IniciarSesion_Juan_Alejandro.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
