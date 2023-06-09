926d933b72818176cdf5f181aaa92720874bd47b
jalejandro98 <juan-af98@hotmail.com>
2023-06-09 16:49:16 -0500

    Juan Alejandro: 16:49

---
+++ app/src/main/java/uta/fisei/pizarra/IniciarSesion_Juan_Alejandro.java
@@ -0,0 +1,14 @@
+package uta.fisei.pizarra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IniciarSesion_Juan_Alejandro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion_juan_alejandro);
+    }
+}
