18895c8a407a22be80534a9a03bb38b6451e0a7c
jalejandro98 <juan-af98@hotmail.com>
2023-06-09 17:30:26 -0500

    Juan Alejandro: 17:30

--- app/src/main/java/uta/fisei/pizarra/IniciarSesion_Juan_Alejandro.java
+++ app/src/main/java/uta/fisei/pizarra/IniciarSesion_Juan_Alejandro.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IniciarSesion_Juan_Alejandro extends AppCompatActivity {
 
     private Button button_ingresar;
+    private EditText et1;
+    private EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,16 @@ public class IniciarSesion_Juan_Alejandro extends AppCompatActivity {
         setContentView(R.layout.activity_iniciar_sesion_juan_alejandro);
 
         button_ingresar = findViewById(R.id.button_JUAN);
+        et1 = findViewById(R.id.editText1_JUAN);
+        et2 = findViewById(R.id.editText2_JUAN);
+
     }
 
-    private void onClickIngresar(){
+    public void onClickIngresar(View view){
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        if (et1.equals("Juan") && et2.equals("123")
+         || et1.equals("Mario") && et2.equals("12345")) {
+            startActivity(intent);
+        }
     }
 }
